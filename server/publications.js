619d858d057df91a5c67c9ac887622b6bb6007bf
Sid Shanker <sshanker220@gmail.com>
2014-02-08 10:32:55 -0500

    quick updates

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,7 @@ Meteor.publish('workspaces', function() {
 Meteor.publish('files', function() {
   return Files.find({userId: this.userId});
 });
+
+Meteor.publish('hints', function() {
+  return Hints.find({userId: this.userId});
+});
